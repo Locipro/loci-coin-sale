68d1147bb63ec8929c8d2b04cad72057f06a95e7
Dan Emmons <demmons@Dans-Mac-Pro.local>
2018-01-17 05:07:23 -0600

    trying new approach

--- migrations/4_deploy_post_sale_reddit_dist.js
+++ migrations/4_deploy_post_sale_reddit_dist.js
@@ -3,7 +3,10 @@ const Ownable = artifacts.require('zeppelin-solidity/contracts/ownership/Ownable
 const Contactable = artifacts.require('zeppelin-solidity/contracts/ownership/Contactable.sol');
 const LOCIcoin = artifacts.require("./LOCIcoin.sol");
 const LOCIsale = artifacts.require("./LOCIsale.sol");
-const LOCIreddit = artifacts.require("./LOCIreddit.sol");
+const LOCIreddit1 = artifacts.require("./LOCIreddit1.sol");
+const LOCIreddit2 = artifacts.require("./LOCIreddit2.sol");
+const LOCIreddit3 = artifacts.require("./LOCIreddit3.sol");
+const LOCIreddit4 = artifacts.require("./LOCIreddit4.sol");
 
 const BigNumber = require('bignumber.js');
 
@@ -24,41 +27,14 @@ module.exports = (deployer, network, accounts) => {
     deployer.deploy(Contactable, {from: deployAddress});
     
     console.log('linking Ownable SafeMath Contactable');
-    deployer.link(Ownable, [LOCIreddit], {from: deployAddress});
-    deployer.link(SafeMath, [LOCIreddit], {from: deployAddress});
-    deployer.link(Contactable, [LOCIreddit], {from: deployAddress});
+    deployer.link(Ownable, [LOCIreddit1, LOCIreddit2, LOCIreddit3, LOCIreddit4], {from: deployAddress});
+    deployer.link(SafeMath, [LOCIreddit1, LOCIreddit2, LOCIreddit3, LOCIreddit4], {from: deployAddress});
+    deployer.link(Contactable, [LOCIreddit1, LOCIreddit2, LOCIreddit3, LOCIreddit4], {from: deployAddress});    
     
-    let coinOwner = "0x1e35343c175038a8aac75a391524c14a01b1a1b0";
-    let bonusTokenSupply = 10000000000000000000000000; // ten million should do it.
-    let bonusTokenDistribution;
-
     console.log('deploying LOCIreddit');      
-    deployer.deploy(LOCIreddit, LOCIcoin.address, 'LOCIpro.com', {from: deployAddress}).then(() => {        
-        
-        LOCIreddit.deployed().then(function(_bonusTokenDistribution) {
-            console.log( 'assigning bonusTokenDistribution instance' );
-            bonusTokenDistribution = _bonusTokenDistribution;
-            return LOCIcoin.deployed();
-        }).then( function(coinInstance){            
-            console.log( 'assigning coin instance' );                   
-            coin = coinInstance;
-            return coin.ownerSetOverride(bonusTokenDistribution.address, true, {from: deployAddress});
-        }).then( function(resultOfOverride) {
-            console.log('resultOfOverride:' + resultOfOverride);                    
-            return coin.transfer(bonusTokenDistribution.address, bonusTokenSupply, {from: deployAddress});
-        }).then( function(resultOfTransfer) {
-            console.log('resultOfTransfer:' + resultOfTransfer);
-            return coin.balanceOf(LOCIcoin.address);
-        }).then( function(resultOfBalanceForLOCIcoinAddress) {
-            console.log('resultOfBalanceForLOCIcoinAddress:' + resultOfBalanceForLOCIcoinAddress);
-            return coin.balanceOf(bonusTokenDistribution.address);
-        }).then( function(resultOfBalanceForBonusTokenDistributionAddress) {
-            console.log('resultOfBalanceForBonusTokenDistributionAddress:' + resultOfBalanceForBonusTokenDistributionAddress);                    
-            return coin.contactInformation.call();
-        }).then( function(resultOfContactInformation) {
-            console.log('resultOfContactInformation:' + resultOfContactInformation);                                        
-        });
-                    
-    });
+    deployer.deploy(LOCIreddit1, LOCIcoin.address, 'LOCIpro.com', {from: deployAddress});
+    //deployer.deploy(LOCIreddit2, LOCIcoin.address, 'LOCIpro.com', {from: deployAddress});
+    //deployer.deploy(LOCIreddit3, LOCIcoin.address, 'LOCIpro.com', {from: deployAddress});
+    //deployer.deploy(LOCIreddit4, LOCIcoin.address, 'LOCIpro.com', {from: deployAddress});
     
 };
